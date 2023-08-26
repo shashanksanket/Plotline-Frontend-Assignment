627e9f1103c2b10ed28f16d1727e47018416a3f6
shashanksanket <shashank.sanket014@gmail.com>
2023-08-26 15:23:42 +0530

    added basic structure of the project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Form from './pages/Form/form';
+import './App.css'
+
+
+const App = () => {
+    return (
+        <div className='container'>
+            <Form />
+        </div>
+    );
+};
+
+export default App;
+
