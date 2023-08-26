bf42d9c4f04805c823b5633c0a80eff1da7e194b
shashanksanket <shashank.sanket014@gmail.com>
2023-08-26 19:51:25 +0530

    added input for image

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,24 +1,33 @@
-import React, {  } from 'react';
+import React, { } from 'react';
 import './index.css';
 
 
 
 const Button = ({ text, data }) => {
-	return (
-		<>
-			<div className="button-container">
-				<div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
-					<button className="button tooltip">{text}</button>
-					<img className='arrow' style={{ width: "20px" ,height: "10px" }} src="/arrow.svg" alt="arrow" />
-					<div
-						className="tooltip-content"
-						style={{ color: data.textcolor, fontSize: data.textsize, padding: data.padding, backgroundColor: data.bgcolour, borderRadius: data.cornerradius, width: data.tooltipwidth }}>
-						{data.tooltiptext || "Your Text Here"}
-					</div>
-				</div>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <div className="button-container">
+                <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+                    <button className="button tooltip">{text}</button>
+                    <img className='arrow' style={{ width: "20px", height: "10px" }} src="/assets/arrow.svg" alt="arrow" />
+                    <div
+                        className="tooltip-content"
+                        style={{ color: data.textcolor, fontSize: data.textsize, padding: data.padding, backgroundColor: data.bgcolour, borderRadius: data.cornerradius, width: data.tooltipwidth }}>
+                        {data.tooltiptext || "Your Text Here"}
+                        <div style={{display:"block"}}>
+                            {data.imageFile && (
+                                <img
+                                    src={data.imageFile}
+                                    alt="Uploaded"
+                                    style={{ maxWidth: '10%', maxHeight: '10%' }}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
 export default Button;
 
--- src/pages/Form/form.js
+++ src/pages/Form/form.js
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './form.css';
 import Screen from '../screen/screen'
 
@@ -13,8 +13,8 @@ const Form = () => {
     const [tooltipwidth, setTooltipWidth] = useState('');
     const [arrowwidth, setArrowWidth] = useState('');
     const [arrowheight, setArrowHeight] = useState('');
-    const [count,setCount] = useState('')
-
+    const [count, setCount] = useState('')
+    const [imageFile, setImageFile] = useState(null);
     useEffect(() => {
         const storedData = JSON.parse(localStorage.getItem(selectedButton));
         setTooltipText(storedData?.tooltiptext || 'Your Text Here');
@@ -26,9 +26,10 @@ const Form = () => {
         setTooltipWidth(storedData?.tooltipwidth || 'max-content');
         setArrowWidth(storedData?.arrowwidth || '20px');
         setArrowHeight(storedData?.arrowheight || '22px');
+        setImageFile(storedData?.imageFile || null);
     }, [selectedButton]);
     const handleInputChange = (event, fieldName, stateUpdater) => {
-        setCount(count+1)
+        setCount(count + 1)
         stateUpdater(event.target.value);
         const updatedData = {
             ...JSON.parse(localStorage.getItem(selectedButton)),
@@ -36,6 +37,26 @@ const Form = () => {
         };
         localStorage.setItem(selectedButton, JSON.stringify(updatedData));
     };
+    const handleImageUpload = (event) => {
+        const uploadedFile = event.target.files[0];
+        setImageFile(uploadedFile);
+
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const imageData = e.target.result;
+            const updatedData = {
+                ...JSON.parse(localStorage.getItem(selectedButton)),
+                imageFile: imageData,
+            };
+            localStorage.setItem(selectedButton, JSON.stringify(updatedData));
+        };
+        reader.readAsDataURL(uploadedFile);
+        setCount(count + 1);
+    };
+
+
+
+
 
     return (
         <>
@@ -44,60 +65,79 @@ const Form = () => {
                     <div className='form-input'>
                         <label htmlFor="targetElement">Target Element</label>
                         <select value={selectedButton} onChange={(e) => setSelectedButton(e.target.value)}>
-                        <option value="Button 1">Button 1</option>
-                        <option value="Button 2">Button 2</option>
-                        <option value="Button 3">Button 3</option>
-                        <option value="Button 4">Button 4</option>
-                        <option value="Button 5">Button 5</option>
-                    </select>
+                            <option value="Button 1">Button 1</option>
+                            <option value="Button 2">Button 2</option>
+                            <option value="Button 3">Button 3</option>
+                            <option value="Button 4">Button 4</option>
+                            <option value="Button 5">Button 5</option>
+                        </select>
                     </div>
                 </div>
                 <div className='form-group'>
                     <div className='form-input'>
                         <label htmlFor="tooltipText">Tooltip text</label>
-                        <input id="tooltipTextInput" value={tooltiptext} onChange={(e) => handleInputChange(e, 'tooltiptext',setTooltipText)} />
+                        <input id="tooltipTextInput" value={tooltiptext} onChange={(e) => handleInputChange(e, 'tooltiptext', setTooltipText)} />
                     </div>
                 </div>
                 <div className='form-group'>
                     <div className='form-input'>
                         <label htmlFor="textSize">Text Size</label>
-                        <input id="textSizeInput" value={textsize} onChange={(e) => handleInputChange(e, 'textsize',setTextSize)} />
+                        <input id="textSizeInput" value={textsize} onChange={(e) => handleInputChange(e, 'textsize', setTextSize)} />
                     </div>
                     <div className='form-input'>
                         <label htmlFor="padding">Padding</label>
-                        <input id="paddingInput" value={padding} onChange={(e) => handleInputChange(e, 'padding',setPadding)} />
+                        <input id="paddingInput" value={padding} onChange={(e) => handleInputChange(e, 'padding', setPadding)} />
                     </div>
                 </div>
                 <div className='form-group'>
                     <div className='form-input'>
                         <label htmlFor="textColour">Text Colour</label>
-                        <input id="textColourInput" value={textcolour} onChange={(e) => handleInputChange(e, 'textcolour',setTextColour)} />
+                        <input id="textColourInput" value={textcolour} onChange={(e) => handleInputChange(e, 'textcolour', setTextColour)} />
                     </div>
                 </div>
                 <div className='form-group'>
                     <div className='form-input'>
                         <label htmlFor="bgColour">Background colour</label>
-                        <input id="bgColourInput" value={bgcolour} onChange={(e) => handleInputChange(e, 'bgcolour',setBgColour)} />
+                        <input id="bgColourInput" value={bgcolour} onChange={(e) => handleInputChange(e, 'bgcolour', setBgColour)} />
                     </div>
                 </div>
                 <div className='form-group'>
                     <div className='form-input'>
                         <label htmlFor="cornerRadius">Corner radius</label>
-                        <input id="cornerRadiusInput" value={cornerradius} onChange={(e) => handleInputChange(e, 'cornerradius',setCornerRadius)} />
+                        <input id="cornerRadiusInput" value={cornerradius} onChange={(e) => handleInputChange(e, 'cornerradius', setCornerRadius)} />
                     </div>
                     <div className='form-input'>
                         <label htmlFor="tooltipWidth">Tooltip width</label>
-                        <input id="tooltipWidthInput" value={tooltipwidth} onChange={(e) => handleInputChange(e, 'tooltipwidth',setTooltipWidth)} />
+                        <input id="tooltipWidthInput" value={tooltipwidth} onChange={(e) => handleInputChange(e, 'tooltipwidth', setTooltipWidth)} />
                     </div>
                 </div>
                 <div className='form-group'>
                     <div className='form-input'>
                         <label htmlFor="arrowWidth">Arrow width</label>
-                        <input id="arrowWidthInput" value={arrowwidth} onChange={(e) => handleInputChange(e, 'arrowwidth',setArrowWidth)} />
+                        <input id="arrowWidthInput" value={arrowwidth} onChange={(e) => handleInputChange(e, 'arrowwidth', setArrowWidth)} />
                     </div>
                     <div className='form-input'>
                         <label htmlFor="arrowHeight">Arrow height</label>
-                        <input id="arrowHeightInput" value={arrowheight} onChange={(e) => handleInputChange(e, 'arrowheight',setArrowHeight)} />
+                        <input id="arrowHeightInput" value={arrowheight} onChange={(e) => handleInputChange(e, 'arrowheight', setArrowHeight)} />
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <div className="form-input">
+                        <label htmlFor="imageUpload">Upload Image</label>
+                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+                            <input
+                                type="file"
+                                id="imageUpload"
+                                accept="image/*"
+                                onChange={(e) => handleImageUpload(e)}
+                            />    {imageFile && (
+                                <img
+                                    src={imageFile}
+                                    alt="Uploaded"
+                                    style={{ maxWidth: '10%', maxHeight: '10%' }}
+                                />
+                            )}
+                        </div>
                     </div>
                 </div>
             </div>
