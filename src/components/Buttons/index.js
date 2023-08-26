0f8bc000b0005e0914caa6f8e8b78e9b6488e5ad
shashanksanket <shashank.sanket014@gmail.com>
2023-08-26 15:36:11 +0530

    test

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,24 +1,24 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from 'react';
 import './index.css';
 
 
 
-const Button = ({ text,data}) => {
+const Button = ({ text, data }) => {
 	useEffect(() => {
-	
-		console.log(data,text)
+
+		console.log(data, text)
 	}, [data]);
 	return (
 		<>
 			<div className="button-container">
-				<div className='kcuhbhi' style={{display:"flex", flexDirection:"column", justifyContent:"center",alignItems:"center"}}>
-				<button className="button tooltip">{text}</button>
-				<img className='arrow' style={{width:data.arrowwidth, height:data.arrowheight}} src="/arrow.svg"/>
-				<div 
-					className="tooltip-content" 
-					style={{color:data.textcolor,fontSize: data.textsize,padding:data.padding,backgroundColor:data.bgcolour,borderRadius:data.cornerradius,width:data.tooltipwidth}}>
-					{data.tooltiptext}
-				</div>
+				<div className='kcuhbhi' style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+					<button className="button tooltip">{text}</button>
+					<img className='arrow' style={{ width: data.arrowwidth, height: data.arrowheight }} src="/arrow.svg" />
+					<div
+						className="tooltip-content"
+						style={{ color: data.textcolor, fontSize: data.textsize, padding: data.padding, backgroundColor: data.bgcolour, borderRadius: data.cornerradius, width: data.tooltipwidth }}>
+						{data.tooltiptext}
+					</div>
 				</div>
 			</div>
 		</>
